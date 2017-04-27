9f698e15d590269de1ce5e9dd5664b52ecc630ee
7215177523 <7215177523@c205-03>
2017-04-27 11:41:09 +0200

    Projektdateien hinzufügen.

---
+++ taxi/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX()
+        {
+            return this.x;
+        }
+        
+        public double getY()
+        {
+            return this.y;
+        }
+    }
+}
---
+++ taxi/Street.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class Street
+    {
+        private Point start;
+        private Point end;
+        private List<Point> listPoints;
+        
+        public Street(List<Point> listPoints)
+        {
+            this.listPoints = listPoints;
+            foreach(Point p in this.listPoints)
+            {
+
+            }
+        }
+    }
+}
